b88ae8c6e76aafd73f2cebc1c4945ed8636862dc
mabaicun <mabaicun@qianxin.com>
2020-07-15 06:21:47 +0800

    add

---
+++ docs/go/基础篇/main/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	defer fmt.Println("defer main")
+	var user = ""
+	go func() {
+		defer func() {
+			fmt.Println("defer caller")
+			if err := recover(); err != nil {
+				fmt.Println("recover success. err:", err)
+			}
+		}()
+		func() {
+			defer func() {
+				fmt.Println("defer here")
+			}()
+			if user == "" {
+				panic("should set user env.")
+			}
+			fmt.Println("after panic")
+		}()
+	}()
+	time.Sleep(1000)
+	fmt.Println("end of main function")
+}
+
+func acc() func(int) int {
+	var x int
+	return func(delta int) int {
+		fmt.Printf("%+v, %+v", &x, x)
+		x += delta
+		return x
+	}
+}
